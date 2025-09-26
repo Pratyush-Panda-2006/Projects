a198acad734ce255e941f4991d0e467876b4abf1
Pratyush-Panda-2006 <pandapratyush221@gmail.com>
2025-09-26 20:32:33 +0530

    feat(foodapp/Responsive): made Home page responsive

--- foodapp/src/Components/Navbar/Navbar.jsx
+++ foodapp/src/Components/Navbar/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         <img src={assets.search_icon} alt="" />
 
         <div className="navbar-search-icon">
-          <img src={assets.basket_icon} alt="" />
+          <img src={assets.basket_icon} alt=""  />     
           <div className="dot"></div>
         </div>
         <button>Sign in</button>
